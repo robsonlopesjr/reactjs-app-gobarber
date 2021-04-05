5177b43e56a3820f2463e143092310bd2071a56c
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-04-05 16:58:55 -0300

    chore: add page perfil

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,5 @@
-import { shade } from 'polished';
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Container = styled.div``;
 
@@ -13,16 +13,13 @@ export const HeaderContent = styled.div`
   margin: 0 auto;
   display: flex;
   align-items: center;
-
   > img {
     height: 80px;
   }
-
   button {
     margin-left: auto;
     background: transparent;
     border: 0;
-
     svg {
       color: #999591;
       width: 20px;
@@ -35,25 +32,25 @@ export const Profile = styled.div`
   display: flex;
   align-items: center;
   margin-left: 80px;
-
   img {
     width: 56px;
     height: 56px;
     border-radius: 50%;
   }
-
   div {
     display: flex;
     flex-direction: column;
     margin-left: 16px;
     line-height: 24px;
-
     span {
       color: #f4ede8;
     }
-
-    strong {
+    a {
+      text-decoration: none;
       color: #ff9000;
+      &:hover {
+        opacity: 0.8;
+      }
     }
   }
 `;
@@ -67,23 +64,19 @@ export const Content = styled.main`
 export const Schedule = styled.div`
   flex: 1;
   margin-right: 120px;
-
   h1 {
     font-size: 36px;
   }
-
   p {
     margin-top: 8px;
     color: #ff9000;
     display: flex;
     align-items: center;
-    font-weight: 500px;
-
+    font-weight: 500;
     span {
       display: flex;
       align-items: center;
     }
-
     span + span::before {
       content: '';
       width: 1px;
@@ -96,13 +89,11 @@ export const Schedule = styled.div`
 
 export const NextAppointment = styled.div`
   margin-top: 64px;
-
   > strong {
     color: #999591;
     font-size: 20px;
     font-weight: 400;
   }
-
   div {
     background: #3e3b47;
     display: flex;
@@ -111,34 +102,30 @@ export const NextAppointment = styled.div`
     border-radius: 10px;
     margin-top: 24px;
     position: relative;
-
     &::before {
+      content: '';
       position: absolute;
       height: 80%;
       width: 1px;
       left: 0;
       top: 10%;
-      content: '';
       background: #ff9000;
     }
-
     img {
       width: 80px;
       height: 80px;
       border-radius: 50%;
+      background: #ff9000;
     }
-
     strong {
       margin-left: 24px;
       color: #fff;
     }
-
     span {
       margin-left: auto;
       display: flex;
       align-items: center;
       color: #999591;
-
       svg {
         color: #ff9000;
         margin-right: 8px;
@@ -149,7 +136,6 @@ export const NextAppointment = styled.div`
 
 export const Section = styled.section`
   margin-top: 48px;
-
   > strong {
     color: #999591;
     font-size: 20px;
@@ -159,7 +145,6 @@ export const Section = styled.section`
     padding-bottom: 16px;
     margin-bottom: 16px;
   }
-
   > p {
     color: #999591;
   }
@@ -168,43 +153,39 @@ export const Section = styled.section`
 export const Appointment = styled.div`
   display: flex;
   align-items: center;
-
   & + div {
     margin-top: 16px;
   }
-
   span {
     margin-left: auto;
     display: flex;
     align-items: center;
     color: #f4ede8;
     width: 70px;
-
     svg {
       color: #ff9000;
       margin-right: 8px;
     }
   }
-
   div {
     flex: 1;
     background: #3e3b47;
     display: flex;
     align-items: center;
-    padding: 16px 24px;
+    padding: 16px;
     border-radius: 10px;
     margin-left: 24px;
-
     img {
       width: 56px;
       height: 56px;
       border-radius: 50%;
+      background: #ff9000;
     }
-
     strong {
-      margin-left: 24px;
-      color: #fff;
+      margin-left: 16px;
+      color: #f4ede8;
       font-size: 20px;
+      font-weight: 500;
     }
   }
 `;
